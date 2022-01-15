ad59f076773f726001896e6b48012951b8dca132
Daniel Mrva <mrva.paints@gmail.com>
2022-01-14 20:16:26 -0700

    added timer to JS, HTML has blank questions

--- script.js
+++ script.js
@@ -16,16 +16,49 @@
 
 //var for inputs
 
+
 //var for time
+var timerEl = document.querySelector(".Timer");
+
+var timeRemaining = 60;
+
+function countdown() {
+    var timerInterval = setInterval(function() {
+        timeRemaining--;
+        timerEl.textContent = `${timeRemaining} seconds.`
+
+        if (timeRemaining === 0) {
+            clearInterval(timerInterval);
+
+            endGame();
+        }
+    }, 1000);
+}
+
 //var for score
+
+//shuffles ul li on each question
+// var ul = document.querySelectorAll('ul');
+// for (var i = this.ul.children.length; i >= 0; i--) {
+//     this.ul.appendChild(this.ul.children[Math.random() * i | 0]);
+// }
+//Doesn't appear to work with multiple uls?...
 
 //functions
 
-//answer checker.  Use .correct .incorrect classes in answers.  
+//answer checker.  Use .true .false classes in answers.  
     //event listener keyed to clicks; either get values from HTML or listener can be keyed to ".correct" or ".incorrect"
     //correct answers ++ to score and change style of current question to Display: None, and change Display property of next question.
     //incorrect answers -5s to time and advance to next question just like correct answers
     //consider using data-set?  Could probably switch the display property that way, possibly number questions?
+
+
+//start game function
+function starGame() {
+    countdown();
+}
+
+//end game function?
 
 //timer
 
