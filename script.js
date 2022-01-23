825cc3f9d07b90df053fa754883d4f6a7f869619
Daniel Mrva <mrva.paints@gmail.com>
2022-01-23 15:03:34 -0700

    have question advancement working, have answer checker working, but socre does not advance (maybe it is not working?), have question render working, as well as question clearing function.  Error when questions reach the end.

--- script.js
+++ script.js
@@ -27,9 +27,27 @@ var timeRemaining = 60;
 
 //var for question tracking
 var totalQuestions = 10;
-var questionsRemaining = totalQuestions;
 var questionList = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"];
 var usedQuestions = []
+var questionsRemaining = 10;
+
+//var for rendering html elements
+var container = document.getElementById("question-container");
+var questionText=document.getElementById("question-text")
+var questionH3 = document.createElement("h3");
+var ulEl = document.getElementById("answers");
+// var liEl = document.createElement("li");
+// var a1 = document.getElementById("a1");
+// var a2 = document.getElementById("a2");
+// var a3 = document.getElementById("a3");
+// var a4 = document.getElementById("a4");
+var newLi = document.createElement("li")
+var buttonA1 = document.createElement("button");
+var buttonA1 = document.createElement("button");
+var buttonA1 = document.createElement("button");
+var buttonA1 = document.createElement("button");
+
+
 
 //questions will be in objects, with question number, question, answers, and true answer 
 
@@ -94,10 +112,15 @@ var questions = [
         "answers": ["50", "17", "14", "49"],
         "trueAnswer": "49",
     }
-]
+];
+
+var i = 0;
+
 
 //var for score
+var scoreEl = document.getElementById("score");
 var score = 0;
+scoreEl.innerText = `Current Score: ${score}`;
 
 //functions
 
@@ -113,20 +136,41 @@ function countdown() {
 }
 
 function getQuestion() {
-    var selectedQuestion = questionList[Math.floor(Math.random() * questionList.length)];
-    console.log(selectedQuestion);
+    // var selectedQuestion = questions[Math.floor(Math.random() * questions.length)];
+    var selectedQuestion = questions[i];
+    // console.log(selectedQuestion);
     // console.log(selectedQuestion.number);
     // console.log(selectedQuestion.question);
     // console.log(selectedQuestion.answers);
     // console.log(selectedQuestion.trueAnswer);
-    
-    
+    questionText.innerText = `${selectedQuestion.question}`;
+    for (var index = 0; index < selectedQuestion.answers.length; index++) {
+        var newLi = document.createElement("li");
+        var newBtn = document.createElement("button");
+        newBtn.textContent = `${selectedQuestion.answers[index]}`
+        ulEl.append(newLi);
+        newLi.append(newBtn);
+        newLi.classList.add(`a${index}`)
+        newBtn.classList.add("aButt")
+    }
+    if (i === questions.length) {
+        i = 0;
+        endGame();
+    }
 };
 
 //start game function
 function startGame() {
-    // countdown();
+    countdown();
     getQuestion();
+    startButton.disabled = true;
+}
+
+function clearQuestion () {
+    while (ulEl.firstChild) {
+        ulEl.removeChild(ulEl.firstChild);
+        console.log("element removed");
+    };
 }
 
 function endGame() {
@@ -139,19 +183,29 @@ startButton.addEventListener("click", function() {
     startGame();
 });
 
-// //answer checker.  Use .true .false classes in answers.
-// trueButton.addEventListener("click", function() {
-//     score++;
-//     questionsRemaining --;
-//     getQuestion();
-// });
-
-// falseButton.addEventListener("click", function() {
-//     timeRemaining - 5;
-//     questionsRemaining --;
-//     getQuestion();
-// });
-    //event listener keyed to clicks; either get values from HTML or listener can be keyed to ".correct" or ".incorrect"
-    //correct answers ++ to score and change style of current question to Display: None, and change Display property of next question.
-    //incorrect answers -5s to time and advance to next question just like correct answers
-    //consider using data-set?  Could probably switch the display property that way, possibly number questions?
+ulEl.addEventListener('click', function (e) {
+    // But only alert for elements that have an alert-button class
+    if (e.target.classList.contains("aButt")) {
+    //   console.log(`${e.target.innerHTML} touched the butt`)
+      answerCheck();
+      function answerCheck() {
+        var selectedQuestion = questions[i];
+        if (e.target.innerHTML === selectedQuestion.trueAnswer) {
+          score++;
+          clearQuestion();
+          questions[i++];
+          questionsRemaining--
+          getQuestion();
+        } else {
+          timeRemaining--;
+          clearQuestion();
+          questions[i++];
+          questionsRemaining--
+          getQuestion();
+        }
+      }
+    }
+  });
+  
+//   ask the class, specifically Nick Martin, how he got his questions to itterate through, also ask how to get score to update.
+//  create end game function to store score, initials, etc.
